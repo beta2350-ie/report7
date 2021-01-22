3c709324460377c6f2325998186c9e84682973b9
takumi yoneda <e205714@ie.u-ryukyu.ac.jp>
2021-01-22 19:37:32 +0900

    2nd commit

---
+++ report7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void sortTheScore(int array[],int size);
+void printScore(int array[],int size);
+
+int sortTheScore(int array[],int size){
+    for(int j=0;i<size;i++){
+        
+    }
+}
+void printScore(int array[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d",array[i]);
+    }
+}
+int main(){
+    const int SIZE = 5; //定数として配列のサイズを宣言                          
+    int scores[SIZE] = {0, 60, 70, 100, 90};
+    //処理コード
+    sortTheScore(scores,SIZE);
+    printScore(scores,SIZE);
+    return 0;
+}
